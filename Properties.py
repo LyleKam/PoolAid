dd691b186c05fc8a3daabb783341f2f19b2b0d39
Your Name <you@example.com>
2019-10-12 14:14:25 -0400

    feat: first test file

---
+++ Properties.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class Ball:
+    def __init__(self):
+        self.size = 1
+        self.weight = 2
+    
+class Table:
+    def __init__(self):
+        self.length = 20
+        self.width = 10
+    
